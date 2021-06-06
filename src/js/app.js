699c02fc7c24cabf3b4e0c76cf6e2d709a7353d6
IanSvm <61124274+IanSvm@users.noreply.github.com>
2021-06-06 14:23:22 -0500

    foody a.1

--- src/js/app.js
+++ src/js/app.js
@@ -5,20 +5,25 @@ document.addEventListener('DOMContentLoaded', function () {
 function iniciarApp(){
     mostrarServicios();
 }
-
+//funcion asincrona
 async function mostrarServicios(){
 try {
+    //petición a los datos json
     const resultado = await fetch('./menu.json');
+    //formateando petición
     const db = await resultado.json();
+    //Accediendo a los atributos de menu con Destructuring de la variable db
     const {menu} = db;
     
     //HTML
+    //recorriendo JSON
     menu.forEach(runner =>{
+        //Guardando contenido en variables para cada valor de los atributos;s
         const{id, nombre, precio, img} = runner;
         //console.log(runner);
 
         //DOM Scripting
-        //Nombre
+        //Creando parrafos <p> del nombre
         const nombreComida = document.createElement('P');
         nombreComida.textContent=nombre;
         nombreComida.classList.add('nombre-comida');
@@ -27,18 +32,21 @@ try {
         //Precio
         const precioComida = document.createElement('P');
         // console.log(precioComida);
-        precioComida.textContent= "$ "+precio;
+        precioComida.textContent= "$"+precio;
         precioComida.classList.add('precio-comida');
 
         //Img
         const imgComida = document.createElement('IMG');
         imgComida.setAttribute("src", img)
-        console.log(imgComida);
+        //console.log(imgComida);
 
         //Div contenedor
         const comidaDiv = document.createElement('DIV')
         comidaDiv.classList.add('div-comida');
-        
+        //Asignando dataset a cada contenedor 
+        comidaDiv.dataset.idComida = id;
+        //Agregando event handler al div contenedor de los productos
+        comidaDiv.onclick = seleccionarServicio;
 
         //Precio y nombre al div
         comidaDiv.appendChild(nombreComida);
@@ -52,4 +60,23 @@ try {
 } catch (error) {
     console.log(error);
 }
+}
+
+function seleccionarServicio(e){
+
+    let elemento;
+    //Seleccionar forzosamente el div contenedor(id dataset)
+    if(e.target.tagName === 'P' || e.target.tagName === 'IMG'){
+        elemento= e.target.parentElement;
+    }else{
+        elemento= e.target;
+    }
+
+   
+    if(elemento.classList.contains('selected')){
+        elemento.classList.remove('selected');
+    }else{
+        elemento.classList.add('selected');
+    }
+    
 }
