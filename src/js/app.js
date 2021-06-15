e67eeb599edeb0c6aeb743c5b5b51f947b0ebf21
IanSvm <61124274+IanSvm@users.noreply.github.com>
2021-06-14 21:35:26 -0500

    navegacion

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,73 @@
+let n = 1;
+
+
 document.addEventListener('DOMContentLoaded', function () {
     iniciarApp();
 });
 
 function iniciarApp(){
     mostrarServicios();
+
+    //Resaltar div que presiona
+    mostrarSeccion();
+    //Oculta o muestra sección
+    cambiarSeccion();
+
+    //Paginacion
+    paginaSiguiente();
+    paginaAnterior();
+
+    //Comprueba pag actual para mostrar u ocultar paginación
+    botonesPaginador();
 }
+
+
+function mostrarSeccion(){
+
+    //Eliminar seccion anterior
+    const seccionAnterior = document.querySelector('.mostrar-seccion');
+    if (seccionAnterior){
+        seccionAnterior.classList.remove('mostrar-seccion')
+    }
+    const seccionActual = document.querySelector(`#s-${n}`);
+    seccionActual.classList.add('mostrar-seccion')
+
+    const tabAnterior =  document.querySelector('.tabs .actual');
+    if(tabAnterior){
+        tabAnterior.classList.remove('actual');
+    }
+     //Eliminar tab anteriormente seleecionado
+    
+
+    //Resalta el tab actual
+    const tab=document.querySelector(`[data-tab="${n}"]`);
+    tab.classList.add('actual');
+}
+
+
+
+
+function cambiarSeccion(){
+    const enlaces = document.querySelectorAll('.tabs button');
+
+    enlaces.forEach( (runner) =>{
+        runner.addEventListener('click', (e) =>{
+            e.preventDefault();
+            n = parseInt(e.target.dataset.tab);
+            console.log(n);
+
+
+            //Llamando la función mostrar sección
+            mostrarSeccion();
+            botonesPaginador();
+
+        })
+    })
+}
+
+
+
+
 //funcion asincrona
 async function mostrarServicios(){
 try {
@@ -134,7 +197,43 @@ function deselect(){
     })
     // console.log(divSelected);
 }
+}
 
+function paginaSiguiente(){
+const pags = document.querySelector('#siguiente');
+pags.addEventListener('click', () =>{
+    n++;
+  
+    botonesPaginador();
+})
+}
+
+function paginaAnterior(){
+    const paga = document.querySelector('#anterior');
+    paga.addEventListener('click', () =>{
+        n--;
+        botonesPaginador();
+    })
+
+}
+
+function botonesPaginador(){
+    const pags = document.querySelector('#siguiente');
+    const paga = document.querySelector('#anterior');
+
+    if(n ===1){
+        console.log("se oculta el boton de anterior")
+        paga.classList.add('ocultar');
+    } else if (n===3){
+        pags.classList.add('ocultar');
+        paga.classList.remove('ocultar');
+    }else {
+        paga.classList.remove('ocultar');
+        pags.classList.remove('ocultar');
+    }
+    
+
+    mostrarSeccion();
 }
 
 
